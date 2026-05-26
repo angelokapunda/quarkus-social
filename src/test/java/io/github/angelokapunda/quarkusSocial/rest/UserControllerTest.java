90d650b6cc1cc089175a408a3e0b232711f30e81
angelokapunda <angelokapunda@gmail.com>
2026-05-26 14:11:37 -0300

    :sparkles: Caso de erro de validação testado

--- src/test/java/io/github/angelokapunda/quarkusSocial/rest/UserControllerTest.java
+++ src/test/java/io/github/angelokapunda/quarkusSocial/rest/UserControllerTest.java
@@ -1,16 +1,19 @@
 package io.github.angelokapunda.quarkusSocial.rest;
 
 import io.github.angelokapunda.quarkusSocial.domain.model.User;
+import io.github.angelokapunda.quarkusSocial.rest.dto.CreateUserRequest;
+import io.github.angelokapunda.quarkusSocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
 class UserControllerTest {
@@ -33,6 +36,30 @@ class UserControllerTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("Should return error when json is not valid")
+    public void createUserValidationErrorTest() {
+        var user = new CreateUserRequest();
+        user.setName(null);
+        user.setAge(null);
+
+        var response =
+                given()
+                    .contentType(ContentType.JSON)
+                    .body(user)
+                .when()
+                    .post("/users")
+                .then()
+                    .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors =  response.jsonPath().getList("errors");
+        assertNotNull(errors.getFirst().get("message"));
+
     }
 
 }
