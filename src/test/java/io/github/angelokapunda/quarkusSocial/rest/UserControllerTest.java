31245ac37695acb5d6147c1c5f70c5c130453006
angelokapunda <angelokapunda@gmail.com>
2026-05-26 19:40:15 -0300

    :sparkles: Realização de testes ordenados

--- src/test/java/io/github/angelokapunda/quarkusSocial/rest/UserControllerTest.java
+++ src/test/java/io/github/angelokapunda/quarkusSocial/rest/UserControllerTest.java
@@ -3,11 +3,17 @@ package io.github.angelokapunda.quarkusSocial.rest;
 import io.github.angelokapunda.quarkusSocial.domain.model.User;
 import io.github.angelokapunda.quarkusSocial.rest.dto.CreateUserRequest;
 import io.github.angelokapunda.quarkusSocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -16,9 +22,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserControllerTest {
 
+    @TestHTTPResource("/users")
+    URL apiURL;
+
     @Test
+    @Order(1)
     @DisplayName("Should create an user successfully")
     public void createUserTest() {
         var user = new User();
@@ -30,7 +41,7 @@ class UserControllerTest {
                 .contentType(ContentType.JSON)
                 .body(user)
             .when()
-                .post("/users")
+                .post(apiURL)
             .then()
                 .extract().response();
 
@@ -39,6 +50,7 @@ class UserControllerTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Should return error when json is not valid")
     public void createUserValidationErrorTest() {
         var user = new CreateUserRequest();
@@ -50,7 +62,7 @@ class UserControllerTest {
                     .contentType(ContentType.JSON)
                     .body(user)
                 .when()
-                    .post("/users")
+                    .post(apiURL)
                 .then()
                     .extract().response();
 
@@ -59,6 +71,20 @@ class UserControllerTest {
 
         List<Map<String, String>> errors =  response.jsonPath().getList("errors");
         assertNotNull(errors.getFirst().get("message"));
+
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Should list all users")
+    public void listAllUserTest() {
+        given()
+            .contentType(ContentType.JSON)
+        .when()
+            .get(apiURL)
+        .then()
+            .statusCode(200)
+            .body("size()", Matchers.is(1));
 
     }
 
